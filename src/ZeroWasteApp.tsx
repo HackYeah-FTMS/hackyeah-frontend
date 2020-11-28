cd5b75432a3e7a4679dd310a205f32d2be1e68b2
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2020-11-28 18:33:18 +0100

    Add router for routing different components

--- src/ZeroWasteApp.tsx
+++ src/ZeroWasteApp.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {CSSProperties} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import QuestionsPage from "./pages/QuestionsPage";
+import ResultsPage from "./pages/ResultsPage";
+import HomePage from "./pages/HomePage";
 
-function ZeroWasteApp() {
+
+const ZeroWasteApp = () => {
   return (
-    <div>
-      <header>Hello app</header>
+      <div style={{height: '100vh'}}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/questions" component={QuestionsPage} />
+          <Route path="/result" component={ResultsPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default ZeroWasteApp;
