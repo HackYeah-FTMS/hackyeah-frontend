da132427790c6a3aff6554415fa5cf997ebcb0de
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2020-11-28 23:00:35 +0100

    Adjust HomePage styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -23,9 +23,16 @@ const HomePage = () => {
             Odpowiedz na kilka pytań, a powiemy Ci co powinieneś zrobić! :)
           </Text>
         </WelcomeText>
-        <StyledButton onClick={() => history.push("/questions")}>
+        <Button
+          style={{
+            background: "white",
+            color: "#ff6a00",
+            fontWeight: "bold"
+          }}
+          onClick={() => history.push("/questions")}
+        >
           Zaczynamy!
-        </StyledButton>
+        </Button>
       </BorderBox>
     </BaseContainer>
   );
@@ -41,20 +48,5 @@ const WelcomeText = styled.div.attrs(() => ({}))`
   color: white;
   padding-bottom: 75px;
 `;
-
-const StyledButton = withStyles({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: 3,
-    border: 0,
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  },
-  label: {
-    textTransform: "capitalize",
-  },
-})(Button);
 
 export default HomePage;
