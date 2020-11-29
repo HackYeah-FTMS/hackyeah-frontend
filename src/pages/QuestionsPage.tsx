20c7a0b266d0d8b49b0eac4a30e26dc443fbee02
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2020-11-29 02:14:30 +0100

    Add more options for questions

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -6,26 +6,138 @@ import BaseContainer from "../components/BaseContainer";
 import styled from "styled-components";
 
 const QuestionsPage = () => {
-  const questions: Array<String> = [
-    "Czy Twoj przedmiot jest z jednolitego tworzywa?",
-    "Czy Twoj przedmiot przechowywał produkty żywnościowe płynne?",
-    "Czy Twoj przedmiot jest z plastiku?",
-  ];
-  const [questionNumber, setQuestionNumber] = useState(0);
+  const generalQuestions = () => {
+    let map: Map<String, Array<any>> = new Map<String, Array<any>>();
+    map.set("Czy jest to przedmiot papierowy?", ["PAPER", "GLASS"]);
+    map.set("Czy przedmiot jest wykonany ze szkła?", ["GLASS", "BIO"]);
+    map.set("Czy jest to odpad bio?", ["BIO", "METAL_PLASTIC"]);
+    map.set("Czy jest to tworzywo sztuczne bądź metal?", [
+      "METAL_PLASTIC",
+      "OTHERS",
+    ]);
+    return map;
+  };
+
+  const paperQuestions = () => {
+    let map: Map<any, Array<any>> = new Map<any, Array<any>>();
+    map.set("Czy są to chusteczki bądź ręcznik papierowy?", [
+      "END_ALL",
+      "PAPER",
+    ]);
+    map.set("Czy materiał jest zatłuszczony?", ["END_ALL", "PAPER"]);
+    map.set("Czy przedmiot jest mokry lub brudny?", ["END_ALL", "PAPER"]);
+    map.set("Czy są to gazety?", ["END_PAPER", "PAPER"]);
+    map.set("Czy są to zeszyty bądź książki?", ["END_PAPER", "PAPER"]);
+    return map;
+  };
+
+  const glassQuestions = () => {
+    let map: Map<any, Array<any>> = new Map<any, Array<any>>();
+    map.set("Czy jest to opakowanie po kosmetykach?", ["END_ALL", "GLASS"]);
+    map.set("Czy jest to butelka lub słoik?", ["END_ALL", "GLASS"]);
+    map.set("Czy jest to żarówka lub lampa?", ["END_OTHERS", "GLASS"]);
+    return map;
+  };
+
+  const bioQuestions = () => {
+    let map: Map<any, Array<any>> = new Map<any, Array<any>>();
+    map.set("Czy są to kości?", ["END_ALL", "BIO"]);
+    map.set("Czy są to resztki jedzenia?", ["END_BIO", "BIO"]);
+    map.set("Czy są to fusy po herbacie?", ["END_BIO", "BIO"]);
+    map.set("Czy są to odpaki warzywne lub owocowe?", ["END_BIO", "BIO"]);
+    return map;
+  };
+
+  const metalOrPlasticQuestions = () => {
+    let map: Map<any, Array<any>> = new Map<any, Array<any>>();
+    map.set("Czy są to plastikowe worki?", [
+      "END_METAL_PLASTIC",
+      "METAL_PLASTIC",
+    ]);
+    map.set("Czy są to puszki?", ["END_METAL_PLASTIC", "METAL_PLASTIC"]);
+    map.set("Czy są to plastikowe butelki?", [
+      "END_METAL_PLASTIC",
+      "METAL_PLASTIC",
+    ]);
+    map.set("Czy są to opakowania zabrudzone tłuszczem?", [
+      "END_OTHERS",
+      "METAL_PLASTIC",
+    ]);
+    return map;
+  };
+
+  const questionsByTypes = () => {
+    let map: Map<String, Map<String, Array<String>>> = new Map<
+      String,
+      Map<String, Array<String>>
+    >();
+    map.set("GENERAL", generalQuestions());
+    map.set("PAPER", paperQuestions());
+    map.set("GLASS", glassQuestions());
+    map.set("BIO", bioQuestions());
+    map.set("METAL_PLASTIC", metalOrPlasticQuestions());
+    return map;
+  };
 
   const history = useHistory();
-  const nextQuestion = () => {
-    if (questionNumber + 1 < questions.length) {
-      setQuestionNumber(questionNumber + 1);
+
+  const [actualQuestionIndex, setActualQuestionIndex] = useState(0);
+  const [actualQuestionCategory, setActualQuestionCategory] = useState<String>(
+    "GENERAL"
+  );
+  const [actualQuestion, setActualQuestion] = useState<String>(
+    "Czy jest to przedmiot papierowy?"
+  );
+
+  const goToNextQuestion = (answer: String) => {
+    const [index, questionCategory] = checkPreviousAnswer(answer);
+    if (questionCategory && new String(questionCategory).includes("END_")) {
+      history.push("/result?action=" + questionCategory);
     } else {
-      history.push("/result");
+      assignNewQuestion(index, questionCategory);
     }
+  };
+
+  const checkPreviousAnswer = (answer: String) => {
+    const categoryQuestions: Map<
+      String,
+      Array<String>
+    > = questionsByTypes().get(actualQuestionCategory)!!;
+    const categoryQuestionsNumber = categoryQuestions.size;
+    if (categoryQuestionsNumber === actualQuestionIndex + 1) {
+      return [actualQuestionIndex, "END_OTHERS"];
+    }
+    const answerActions = categoryQuestions.get(actualQuestion)!!;
+    const nextQuestionCategory =
+      answer === "YES"
+        ? (answerActions[0] as String)
+        : (answerActions[1] as String);
+    let questionIndex = actualQuestionIndex;
+    let questionCategory = actualQuestionCategory;
+    if (nextQuestionCategory !== actualQuestionCategory) {
+      questionIndex = 0;
+      questionCategory = nextQuestionCategory;
+    } else {
+      questionIndex = actualQuestionIndex + 1;
+    }
+    return [questionIndex, questionCategory];
+  };
+
+  const assignNewQuestion = (index: any, questionCategory: any) => {
+    const categoryQuestions: Map<
+      String,
+      Array<String>
+    > = questionsByTypes().get(questionCategory)!!;
+    const question = Array.from(categoryQuestions.keys())[index];
+    setActualQuestionCategory(questionCategory);
+    setActualQuestionIndex(index);
+    setActualQuestion(question);
   };
 
   return (
     <BaseContainer>
       <DefinedHeightBorderBox>
-        <Question>{questions[questionNumber]}</Question>
+        <Question>{actualQuestion}</Question>
         <ButtonBox>
           <Button
             style={{
@@ -34,7 +146,7 @@ const QuestionsPage = () => {
               fontWeight: "bold",
             }}
             size="large"
-            onClick={nextQuestion}
+            onClick={() => goToNextQuestion("YES")}
           >
             Tak
           </Button>
@@ -46,7 +158,7 @@ const QuestionsPage = () => {
               fontWeight: "bold",
             }}
             size="large"
-            onClick={nextQuestion}
+            onClick={() => goToNextQuestion("NO")}
           >
             Nie
           </Button>
--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -2,17 +2,37 @@ import React from "react";
 import BaseContainer from "../components/BaseContainer";
 import BorderBox from "../components/BorderBox";
 import styled from "styled-components";
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { useLocation } from "react-router-dom";
 
-import yellow from '../assets/images/yellow.png';
-import grey from '../assets/images/grey.png';
-import brown from '../assets/images/brown.png';
-import blue from '../assets/images/blue.png';
-import green from '../assets/images/green.png';
-import {useHistory} from "react-router-dom";
+import yellow from "../assets/images/yellow.png";
+import grey from "../assets/images/grey.png";
+import brown from "../assets/images/brown.png";
+import blue from "../assets/images/blue.png";
+import green from "../assets/images/green.png";
+import { useHistory } from "react-router-dom";
 
+function useQuery() {
+  const rx = new RegExp("[?&]action=([^&]+).*$");
+  const returnVal = useLocation().search.match(rx);
+  return returnVal === null ? "" : returnVal[1];
+}
 const ResultsPage = () => {
   const history = useHistory();
+  let actionType = useQuery();
+  console.log(actionType);
+
+  const mappings = () => {
+    let map: Map<String, String> = new Map<String, any>();
+    map.set("END_METAL_PLASTIC", "Metal");
+    map.set("END_METAL_PLASTIC", "Tworzywa sztuczne");
+    map.set("END_ALL", "Odpady zmieszane");
+    map.set("END_BIO", "Odpady organiczne");
+    map.set("END_PAPER", "Papier");
+    map.set("END_GLASS", "Szkło");
+    return map;
+  };
+
   const getImagesPerType = () => {
     let map: Map<String, any> = new Map<String, any>();
     map.set("Metal", yellow);
@@ -24,18 +44,42 @@ const ResultsPage = () => {
     return map;
   };
 
-  const randomIndex = Math.floor(Math.random() * (getImagesPerType().size - 1));
-  const garbageType = Array.from(getImagesPerType().keys())[randomIndex];
-  const garbageUrl = getImagesPerType().get(garbageType);
+  if (!actionType || actionType === "END_OTHERS") {
+    return (
+      <BaseContainer>
+        <FlexBorderBox>
+          <GoBackSection onClick={() => history.push("/")}>
+            <ArrowBackIosIcon fontSize="large" style={{ color: "#1A81B3" }} />
+            <AgainText>Jeszcze raz</AgainText>
+          </GoBackSection>
+          <ProblemText>
+            <div>
+              Prawdopodobnie próbujesz wyrzucić rzecz która nie kwalifikuje się
+              do bezpośrednije segregacji bądź też algorytm nie znalazł
+              dopasowania.
+            </div>
+            <div>
+              Odpady wielkogabarytowe, odpady niebezpieczne, medyczne, sprzęt
+              elektroniczny lub materiały budowlane nie mogą być wyrzucane do
+              osiedlowych śmietników.
+            </div>
+            <div>
+              Więcej informacji: https://naszesmieci.mos.gov.pl/jak-segregowac
+            </div>
+          </ProblemText>
+        </FlexBorderBox>
+      </BaseContainer>
+    );
+  }
 
+  const garbageType = mappings().get(actionType)!!;
+  const garbageUrl = getImagesPerType().get(garbageType);
   return (
     <BaseContainer>
       <FlexBorderBox>
         <GoBackSection onClick={() => history.push("/")}>
-          <ArrowBackIosIcon fontSize="large" style={{color: "#1A81B3"}}/>
-          <AgainText>
-            Jeszcze raz
-          </AgainText>
+          <ArrowBackIosIcon fontSize="large" style={{ color: "#1A81B3" }} />
+          <AgainText>Jeszcze raz</AgainText>
         </GoBackSection>
         <UpperText>Wyrzuć "śmieci" tutaj:</UpperText>
         <Image src={garbageUrl} />
@@ -50,16 +94,16 @@ const AgainText = styled.div.attrs(() => ({}))`
   flex-direction: column;
   align-items: center;
   font-weight: bold;
-  color: #1A81B3;
+  color: #1a81b3;
   padding-top: 5px;
-`
+`;
 
 const GoBackSection = styled.div.attrs(() => ({}))`
   width: 100%;
   display: flex;
   justify-content: flex-start;
   padding-bottom: 20px;
-`
+`;
 
 const FlexBorderBox = styled(BorderBox)`
   display: flex;
@@ -71,17 +115,22 @@ const FlexBorderBox = styled(BorderBox)`
 const Image = styled.img.attrs(() => ({}))`
   width: 100px;
   height: 150px;
-`
+`;
+const ProblemText = styled.div.attrs(() => ({}))`
+  text-align: center;
+  color: #1a81b3;
+`;
+
 const UpperText = styled.div.attrs(() => ({}))`
   text-align: center;
-  color: #1A81B3;
+  color: #1a81b3;
   padding-bottom: 15px;
   font-size: 20px;
   font-weight: bold;
 `;
 const BottomText = styled.div.attrs(() => ({}))`
   text-align: center;
-  color: #1A81B3;
+  color: #1a81b3;
   padding-top: 15px;
   text-transform: uppercase;
   font-weight: bold;
